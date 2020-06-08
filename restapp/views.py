622cb790afbf982c87c5a704b80e6bd01d719208
kittu <kittu.603@gmail.com>
2020-06-08 16:56:45 +0530

    added comments and API endpoints for due and compeleted tasks

--- restapp/views.py
+++ restapp/views.py
@@ -8,13 +8,13 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('date_created')
     serializer_class = TaskSerializers
 
-#view for completed tasks
+#view for completed tasks - API endpoint for completed tasks
 
 class TasksCompletedViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('date_created').filter(is_completed = True)
     serializer_class = TaskSerializers
 
-#view for completed tasks
+#view for completed tasks  - API endpoint for due tasks
 
 class TasksDueViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().order_by('date_created').filter(is_completed = False)
